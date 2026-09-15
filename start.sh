4964a32f617f792e45d8f22186727a11eb43a608
wrisk-dev <aunggyugyi53@gmail.com>
2026-09-15 10:13:19 +0630

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir -p /var/run/sshd
+echo 'root:root' | chpasswd
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
+/usr/sbin/sshd -D
